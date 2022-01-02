a5758f085e91ca8c7b4a6b8fbd7815a10c8464f4
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-01-02 19:04:14 -0300

    classe vendedor

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,13 +1,18 @@
-package Aplicacao;
+package aplicacao;
+
+import java.util.Date;
 
 import model.entidades.Departamento;
+import model.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento dp = new Departamento(1, "livros");
+		Vendedor vendedor = new Vendedor(21, "Bob", "Bob@gmail.com", new Date(), 300.00, dp);
 		System.out.println(dp);
+		System.out.println(vendedor);
 
 	}
 
