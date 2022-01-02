1922308eab0cbe853ee1ce1f5478a64074d5b738
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-01-02 17:43:48 -0300

    classe departamento

---
+++ src/Aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package Aplicacao;
+
+import model.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento dp = new Departamento(1, "livros");
+		System.out.println(dp);
+
+	}
+
+}
